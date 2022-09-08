bf725acf6f8e5d99ea5e803b32f50b8bd65e009a
Rodrigo Freire <rodrigofreitasfreire@gmail.com>
2022-09-08 16:37:00 +0100

    Began implementing wordle game

---
+++ commands/games/wordle.js
@@ -0,0 +1,56 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('wordle')
+		.setDescription('Joga uma partida de Wordle!')
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('help')
+                .setDescription('Info acerca de todos os subcomandos associados a /wordle')
+        )
+        .addSubcommand((subcommand) => 
+            subcommand
+                .setName('start')
+                .setDescription('Inicia um novo jogo de Wordle!')
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('delete')
+                .setDescription('Apaga um jogo de Wordle ativo!')
+                .addIntegerOption((option) =>
+                    option
+                        .setName('gameid')
+                        .setDescription('Id do jogo pretendido')
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('answer')
+                .setDescription('Resposta ao jogo')
+                .addStringOption((option) =>
+                    option
+                        .setName('word')
+                        .setDescription('Palavra de resposta')
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('result')
+                .setDescription('Mostra um jogo de Wordle previamente terminado!')
+                .addIntegerOption((option) =>
+                    option
+                        .setName('resultid')
+                        .setDescription('Id do jogo pretendido')
+                        .setRequired(true)
+                )
+        ),
+	async execute(interaction, client) {
+		await interaction.reply({
+            content : 'WIP',
+            ephemeral : true
+        });
+	},
+};
